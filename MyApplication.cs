4608fdfb6f15a61d63b8b927b9e2a352fe490219
Adriano Cruz <adriano.cruz@sinergyrh.com.br>
2022-09-20 17:34:28 -0300

    Commit geral

---
+++ MyApplication.cs
@@ -0,0 +1,39 @@
+using NewsAPI;
+using NewsAPI.Models;
+using NewsAPI.Constants;
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // init with your API key
+            var newsApiClient = new NewsApiClient("94ba4129b2154f739c8769d603519ac9");
+            var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+            {
+                Q = "Apple",
+                SortBy = SortBys.Popularity,
+                Language = Languages.EN,
+                From = new DateTime(2022, 6, 16)
+            });
+            if (articlesResponse.Status == Statuses.Ok)
+            {
+                // total results found
+                Console.WriteLine(articlesResponse.TotalResults);
+                // here's the first 20
+                foreach (var article in articlesResponse.Articles)
+                {
+                    // author
+                    Console.WriteLine(article.Author);
+                    // title
+                    Console.WriteLine(article.Title);                    
+                    // description
+                    Console.WriteLine(article.Description);                    
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ consoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NewsAPI;
+using NewsAPI.Models;
+using NewsAPI.Constants;
+
+namespace consoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            // init with your API key
+            var newsApiClient = new NewsApiClient("94ba4129b2154f739c8769d603519ac9");
+            var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+            {
+                Q = "Apple",
+                SortBy = SortBys.Popularity,
+                Language = Languages.EN,
+                From = new DateTime(2018, 1, 25)
+            });
+            if (articlesResponse.Status == Statuses.Ok)
+            {
+                // total results found
+                Console.WriteLine(articlesResponse.TotalResults);
+                // here's the first 20
+                foreach (var article in articlesResponse.Articles)
+                {
+                    // author
+                    Console.WriteLine(article.Author);
+                    // title
+                    Console.WriteLine(article.Title);                    
+                    // description
+                    Console.WriteLine(article.Description);                    
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
